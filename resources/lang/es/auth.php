6eac1a65c1a39ad81977cce84c5e980aca659b5a
Andrés <bryan.palma02@epn.edu.ec>
2021-07-21 21:37:12 -0500

    a1

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'agree'                     => 'acepto los términos',
+    'already_member'            => 'Ya estoy registrado',
+    'email'                     => 'Dirección de correo electrónico',
+    'failed'                    => 'Estas credenciales no coinciden con nuestros registros.',
+    'forgot_password'           => 'Olvidé mi contraseña',
+    'login'                     => 'Iniciar sesión',
+    'login_facebook'            => 'Iniciar sesión con Facebook',
+    'login_google'              => "Iniciar sesión con Google+",
+    'login_title'               => 'Inicie sesión para iniciar su sesión',
+    'login_twitter'             => 'Iniciar sesión con Twitter',
+    'logout'                    => 'Cerrar sesión',
+    'name'                      => 'Nombre completo',
+    'password'                  => 'Contraseña',
+    'password_confirmation'     => 'Confirmar la contraseña',
+    'register'                  => 'Registrarse',
+    'register_new_member'       => 'Registrar una nueva membresía',
+    'remember_me'               => 'Recuérdame',
+    'remember_password'         => 'Recuerdo mi contraseña volver a iniciar sesión',
+    'reset_password'            => 'Reiniciar',
+    'reset_password_title'      => 'Introduzca su nueva contraseña',
+    'reset_title'               => 'Correo electrónico para restablecer contraseña',
+    'send_password'             => 'Enviar enlace de restablecimiento',
+    'throttle'                  => 'Demasiados intentos de acceso. Por favor intente nuevamente en: segundos segundos. '
+];
